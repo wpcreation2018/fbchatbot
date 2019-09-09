669b4c0da3cc93ead9f7166b46e2173a562c70a3
WP Creation 2018 <43770840+wpcreation2018@users.noreply.github.com>
2019-09-10 00:50:07 +0700

    Move URLs to another file

--- fbchat/controller/bot.py
+++ fbchat/controller/bot.py
@@ -5,31 +5,11 @@ from fbchat.models import *
 from fbchat.models import Message
 
 from var._collect import *
+from var._urls import *
 from functions.response import ResponseBy
 from functions.response import Price
 from functions.response_sku import TwinsShop, ChoiShop
 
-remote_img_urls = {
-    'img_cod'       : 'https://www.theperfectladies.com/wp-content/uploads/tpl-cod-1.png',
-    'img_discount'  : 'https://www.theperfectladies.com/wp-content/uploads/tpl-bigsize-banner.jpg',
-    'img_claim'     : 'https://www.theperfectladies.com/wp-content/uploads/tpl-claim.png',
-    'img_transfer'  : 'https://i.ibb.co/rtKXPrD/transfer.png',
-    'img_thxq'      : 'https://i.ibb.co/6R9Hs0X/thxq.png'
-}
-
-local_img_urls = {
-    'img_cod'       : 'imgs/cod.png',
-    'img_discount'  : 'imgs/discount.jpg',
-    'img_claim'     : 'imgs/claim.png',
-    'img_transfer'  : 'imgs/transfer.png',
-    'img_thxq'      : 'imgs/thxq.png'
-}
-
-tpl_urls = {
-    'home_page'     : 'https://www.theperfectladies.com/',
-    'shop_page'     : 'https://www.theperfectladies.com/products/',
-    'bigsize_page'  : 'https://www.theperfectladies.com/product-category/fashion/bigsize/',
-}
 
 # Subclass fbchat.Client and override required methods
 class EchoBot(Client):
