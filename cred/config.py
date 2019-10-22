0aafd0d3e76641571da2e812a95c40f2d162b73c
WP Creation 2018 <43770840+wpcreation2018@users.noreply.github.com>
2019-10-22 14:12:33 +0700

    Remove content

--- cred/config.py
+++ cred/config.py
@@ -1,7 +1,1 @@
-username_pen = "0614452880"
-username_mik = "poohpooorathai@gmail.com"
-username_san = "joodmai001@gmail.com"
 
-password_pen = b"JFBvb2hjaG9tcG9vMiM="
-password_mik = b"UG9vaDEyMjgwOCM="
-password_san = b"JDE5MjhTNG5wXg=="
